1bad06667260ac8de3250d7db96499bdbe18fa0a
mdjeghbala <mohamed.djeghbala@outlook.fr>
2024-07-05 15:23:58 +0200

    first commit

---
+++ renderer.js
@@ -0,0 +1,2 @@
+const information = document.getElementById('info')
+information.innerText = `Cette application utilise Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), et Electron (v${versions.electron()})`
